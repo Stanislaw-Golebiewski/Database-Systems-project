69f56b0d5d6e9e16c95df76c9833f1a12dd33306
Loffciam zwierzaki <kamil@macron.pl>
2017-01-13 13:13:46 +0100

    Dobry login page

--- pages/login_page.php
+++ pages/login_page.php
@@ -1,69 +1,28 @@
 <?php
-$error = "";
-include("../php/login.php");
-if(isset($_SESSION['login_user'])){
-header("location: ../php/profile.php");
-}
+  $error = "";
+  include("../php/login.php");
+  if(isset($_SESSION['login_user'])) {
+    header("location: ../php/profile.php");
+  }
 ?>
 
 <!DOCTYPE HTML>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="css/login_page.css">
-<style>
-  /*
-    #login{
-      border-radius: 5px;
-      background-color: #f2f2f2;
-      padding: 20px;
-      vertical-align: middle;
-      border: 2px solid black;
-    }
-    body{
-      background-color: #afb5bf;
-    }
-    #error{
-      color: red;
-      text-align: center;
-    }
-
-
-    input[type=submit]{
-      width: 100%;
-      background-color: #4CAF50;
-      color: white;
-      padding: 14px 20px;
-      margin: 8px 0;
-      border: none;
-      border-radius: 4px;
-      cursor: pointer;
-    }
-
-    input[type=submit]:hover{
-      background-color: #45a049;
-    }
-
-    input[type=text], input[type=password]{
-      width: 100%;
-      padding: 12px 20px;
-      margin: 8px 0;
-      display: inline-block;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      box-sizing: border-box;
-    }
-    */
-  </style>
+  <title>DuczBase</title>
+  <link rel="stylesheet" type="text/css" href="style_main.css">
 </head>
 <body>
-
-<div id="login">
-<form action="" method="post">
-<input type="text" name="username" placeholder="username"><br>
-<input type="password" name="password" placeholder="*********"><br>
-<input type="submit" name="submit" value=" LoginTej "><br>
-<div id="error"> <span> <?php echo $error; ?> </sapn> </div>
-</form>
+<div id="logo_box">
+  <div id="logo_big">DuczBase</div>
+  <div id="login_box">
+    <form action="" method="post">
+      <input type="text" name="username" placeholder="username"><br>
+      <input type="password" name="password" placeholder="*********"><br>
+      <input type="submit" name="submit" value=" LoginTej "><br>
+      <div id="error"> <?php echo $error; ?> </div>
+    </form>
+  </div>
 </div>
 </body>
 </html>
